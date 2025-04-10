f2d7b9d1534da25b915a522a80067258fd0a8e4f
@lalkrishna.udayakumar <lalkrishnaudayakumar40@gmail.com>
2025-04-10 23:47:25 +0530

    main updated wioth hevic and providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,49 @@
-void main() async {}
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
+import 'package:slate/src/core/theme/app_theme.dart';
+import 'package:slate/src/data/model/account.dart';
+import 'package:slate/src/data/model/expense.dart';
+import 'package:slate/src/data/model/income.dart';
+import 'package:slate/src/features/screens/slate_style_guide.dart';
+import 'package:slate/src/provider/account_provider.dart';
+import 'package:slate/src/provider/expense_provider.dart';
+import 'package:slate/src/provider/income_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(AccountAdapter());
+  Hive.registerAdapter(ExpenseAdapter());
+  Hive.registerAdapter(IncomeAdapter());
+
+  await Hive.openBox<Account>('accounts');
+  await Hive.openBox<Expense>('expenses');
+  await Hive.openBox<Income>('incomes');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AccountProvider()),
+        ChangeNotifierProvider(create: (_) => ExpenseProvider()),
+        ChangeNotifierProvider(create: (_) => IncomeProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Slate',
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.system,
+        home: SlateStyleGuide(),
+      ),
+    );
+  }
+}
