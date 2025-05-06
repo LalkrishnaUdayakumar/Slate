d39458bcee1e71d41405293c7eb1595e0ccdfa38
@lalkrishna.udayakumar <lalkrishnaudayakumar40@gmail.com>
2025-05-06 10:22:11 +0530

    images added

--- lib/src/core/constants/app_images.dart
+++ lib/src/core/constants/app_images.dart
@@ -7,4 +7,7 @@ class AppImages {
   static const String logo = '$basePath/logo.png';
   static const String placeholder = '$basePath/placeholder.png';
   static const String noData = '$basePath/no_data.png';
+  static const String petrol = '$basePath/petrol.png';
+  static const String chaya = '$basePath/chaya.png';
+  static const String bike = '$basePath/bike.jpeg';
 }
